c7a4d45296eda7f4c642d786e7eac242c5cd0d31
Anne201 <118590269+Anne201@users.noreply.github.com>
2023-12-01 19:19:26 +0800

    version 1

---
+++ multi02/match_menu_set.js
@@ -0,0 +1,9 @@
+menu_title='10以內加法 對對碰選單';
+menu_items = new Array(
+  '1的加法-1,match_set_1.js'
+, '2的加法-2,match_set_2.js'
+, '3的加法-3,match_set_3.js'
+, '4的加法-4,match_set_4.js'
+, '5/6的加法-5&6,match_set_5.js'
+, '7/8/9的加法-7&8&9,match_set_5.js'
+);

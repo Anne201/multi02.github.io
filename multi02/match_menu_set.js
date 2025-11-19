8f251748b0a329e91e7b4156506ed4d34e37a3cd
Anne201 <118590269+Anne201@users.noreply.github.com>
2025-11-19 20:20:28 +0800

    V2

--- multi02/match_menu_set.js
+++ multi02/match_menu_set.js
@@ -3,7 +3,7 @@ menu_items = new Array(
   '1的加法-1,match_set_1.js'
 , '2的加法-2,match_set_2.js'
 , '3的加法-3,match_set_3.js'
-, '4的加法-4,match_set_4.js'
-, '5/6的加法-5&6,match_set_5.js'
-, '7/8/9的加法-7&8&9,match_set_5.js'
+, '4/5的加法-4&5,match_set_4.js'
+, '6/7的加法-6&7,match_set_5.js'
+, '8/9/0的加法-8&9&0,match_set_6.js'
 );
